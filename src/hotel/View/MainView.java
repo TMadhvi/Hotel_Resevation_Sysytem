2fba70973d71a76f13a0cd8295926b803d8958ce
Madhvi <madhvi1320@gmail.com>
2026-06-04 20:08:54 +0530

    updated main view

--- src/hotel/View/MainView.java
+++ src/hotel/View/MainView.java
@@ -12,7 +12,9 @@ public class MainView {
 		System.out.println("Enter 2 for  Customer");
 		System.out.println("Enter 3 for Booking");
 		System.out.println("Enter 4 for Payment");
-		System.out.println("Enter 5 for Exit....");
+		System.out.println("Enter 5 for Booking History");
+		System.out.println("Enter 6 for Available Rooms");
+		System.out.println("Enter 7 for Exit....");
 
 		return sc.nextInt();
 		
